75e742b46877511ac93dc2dafdad43b918c33d23
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-09 21:33:29 +0600

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,156 @@
+class Program
+{
+    static void Main()
+    {
+        Soldier soldier1 = new Soldier("Arthur", 85, 50, 10, 20, 30, 7);
+
+        soldier1.AccessName();
+        soldier1.AccessHealth();
+        soldier1.AccessStrength();
+        soldier1.AccessPower();
+        soldier1.AccessAttack();
+        soldier1.AccessPosition();
+        soldier1.AccessRank();
+        soldier1.AccessLife();
+      Soldier soldier1 = new Soldier("Arthur", 85, 50, 10, 20, 30, 7);
+
+        soldier1.AccessName();
+        soldier1.AccessHealth();
+        soldier1.AccessStrength();
+        soldier1.AccessPower();
+        soldier1.AccessAttack();
+        soldier1.AccessPosition();
+        soldier1.AccessRank();
+        soldier1.AccessLife();
+      v
+        Soldier soldier1 = new Soldier("Arthur", 85, 50, 10, 20, 30, 7);
+
+        soldier1.AccessName();
+        soldier1.AccessHealth();
+        soldier1.AccessStrength();
+        soldier1.AccessPower();
+        soldier1.AccessAttack();
+        soldier1.AccessPosition();
+        soldier1.AccessRank();
+        soldier1.AccessLife();
+      Soldier soldier1 = new Soldier("Arthur", 85, 50, 10, 20, 30, 7);
+
+        soldier1.AccessName();
+        soldier1.AccessHealth();
+        soldier1.AccessStrength();
+        soldier1.AccessPower();
+        soldier1.AccessAttack();
+        soldier1.AccessPosition();
+        soldier1.AccessRank();
+        soldier1.AccessLife();
+      Soldier soldier1 = new Soldier("Arthur", 85, 50, 10, 20, 30, 7);
+
+        soldier1.AccessName();
+        soldier1.AccessHealth();
+        soldier1.AccessStrength();
+        soldier1.AccessPower();
+        soldier1.AccessAttack();
+        soldier1.AccessPosition();
+        soldier1.AccessRank();
+        soldier1.AccessLife();
+      Soldier soldier1 = new Soldier("Arthur", 85, 50, 10, 20, 30, 7);
+
+        soldier1.AccessName();
+        soldier1.AccessHealth();
+        soldier1.AccessStrength();
+        soldier1.AccessPower();
+        soldier1.AccessAttack();
+        soldier1.AccessPosition();
+        soldier1.AccessRank();
+        soldier1.AccessLife();
+     Soldier soldier1 = new Soldier("Arthur", 85, 50, 10, 20, 30, 7);
+
+        soldier1.AccessName();
+        soldier1.AccessHealth();
+        soldier1.AccessStrength();
+        soldier1.AccessPower();
+        soldier1.AccessAttack();
+        soldier1.AccessPosition();
+        soldier1.AccessRank();
+        soldier1.AccessLife();
+      Soldier soldier1 = new Soldier("Arthur", 85, 50, 10, 20, 30, 7);
+
+        soldier1.AccessName();
+        soldier1.AccessHealth();
+        soldier1.AccessStrength();
+        soldier1.AccessPower();
+        soldier1.AccessAttack();
+        soldier1.AccessPosition();
+        soldier1.AccessRank();
+        soldier1.AccessLife();
+      Soldier soldier1 = new Soldier("Arthur", 85, 50, 10, 20, 30, 7);
+
+        soldier1.AccessName();
+        soldier1.AccessHealth();
+        soldier1.AccessStrength();
+        soldier1.AccessPower();
+        soldier1.AccessAttack();
+        soldier1.AccessPosition();
+        soldier1.AccessRank();
+        soldier1.AccessLife();
+      Soldier soldier1 = new Soldier("Arthur", 85, 50, 10, 20, 30, 7);
+
+        soldier1.AccessName();
+        soldier1.AccessHealth();
+        soldier1.AccessStrength();
+        soldier1.AccessPower();
+        soldier1.AccessAttack();
+        soldier1.AccessPosition();
+        soldier1.AccessRank();
+        soldier1.AccessLife();
+      Soldier soldier1 = new Soldier("Arthur", 85, 50, 10, 20, 30, 7);
+
+        soldier1.AccessName();
+        soldier1.AccessHealth();
+        soldier1.AccessStrength();
+        soldier1.AccessPower();
+        soldier1.AccessAttack();
+        soldier1.AccessPosition();
+        soldier1.AccessRank();
+        soldier1.AccessLife();
+      Soldier soldier1 = new Soldier("Arthur", 85, 50, 10, 20, 30, 7);
+
+        soldier1.AccessName();
+        soldier1.AccessHealth();
+        soldier1.AccessStrength();
+        soldier1.AccessPower();
+        soldier1.AccessAttack();
+        soldier1.AccessPosition();
+        soldier1.AccessRank();
+        soldier1.AccessLife();
+      Soldier soldier1 = new Soldier("Arthur", 85, 50, 10, 20, 30, 7);
+
+        soldier1.AccessName();
+        soldier1.AccessHealth();
+        soldier1.AccessStrength();
+        soldier1.AccessPower();
+        soldier1.AccessAttack();
+        soldier1.AccessPosition();
+        soldier1.AccessRank();
+        soldier1.AccessLife();
+      Soldier soldier1 = new Soldier("Arthur", 85, 50, 10, 20, 30, 7);
+
+        soldier1.AccessName();
+        soldier1.AccessHealth();
+        soldier1.AccessStrength();
+        soldier1.AccessPower();
+        soldier1.AccessAttack();
+        soldier1.AccessPosition();
+        soldier1.AccessRank();
+        soldier1.AccessLife();
+      Soldier soldier1 = new Soldier("Arthur", 85, 50, 10, 20, 30, 7);
+
+        soldier1.AccessName();
+        soldier1.AccessHealth();
+        soldier1.AccessStrength();
+        soldier1.AccessPower();
+        soldier1.AccessAttack();
+        soldier1.AccessPosition();
+        soldier1.AccessRank();
+        soldier1.AccessLife();
+      
