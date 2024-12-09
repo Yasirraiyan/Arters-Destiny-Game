2a894b6f28dee2bae1cb62b111e3409abd11fa7b
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-09 21:36:07 +0600

    Create Form3.cs

---
+++ Form3.cs
@@ -0,0 +1,20 @@
+namespace Game
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Game Started");
+        }
+    }
+}
